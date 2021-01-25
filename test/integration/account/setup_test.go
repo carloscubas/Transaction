571ced64da17c655e7b2d8d68dd30758cfebfd25
carloscubas <carlos.cubas@gmail.com>
2021-01-25 00:17:11 -0300

    error unit test fix

--- test/integration/account/setup_test.go
+++ test/integration/account/setup_test.go
@@ -37,12 +37,12 @@ func before() *sql.DB{
 	ddl[1] = "DROP TABLE IF EXISTS OperationsTypes;"
 	ddl[2] = "DROP TABLE IF EXISTS Accounts;"
 	ddl[3] = "CREATE TABLE Accounts (Account_ID INTEGER auto_increment primary key, Document_Number varchar(100) NOT NULL);"
-	ddl[4] = "INSERT INTO Accounts (Document_Number) VALUES ('123456');"
-	ddl[5] = "CREATE TABLE OperationsTypes ( OperationsType_ID INTEGER auto_increment primary key, Description varchar(200) NOT NULL, OperationsType varchar(200) NOT NULL);"
-	ddl[6] = "CREATE TABLE Transactions (Transaction_ID INTEGER auto_increment primary key, Account_ID INTEGER NOT NULL, OperationsType_ID INTEGER NOT NULL, Amount DOUBLE NOT NULL, EventDate DATE);"
-	ddl[7] = "ALTER TABLE Transactions ADD CONSTRAINT Transactions_FK_Account FOREIGN KEY (Account_ID) REFERENCES Accounts(Account_ID);"
-	ddl[8] = "ALTER TABLE Transactions ADD CONSTRAINT Transactions_FK_OperationsType FOREIGN KEY (OperationsType_ID) REFERENCES OperationsTypes(OperationsType_ID);"
-	ddl[9] = "ALTER TABLE Accounts ADD CONSTRAINT Accounts_UN UNIQUE KEY (Document_Number);"
+	ddl[4] = "CREATE TABLE OperationsTypes ( OperationsType_ID INTEGER auto_increment primary key, Description varchar(200) NOT NULL, OperationsType varchar(200) NOT NULL);"
+	ddl[5] = "CREATE TABLE Transactions (Transaction_ID INTEGER auto_increment primary key, Account_ID INTEGER NOT NULL, OperationsType_ID INTEGER NOT NULL, Amount DOUBLE NOT NULL, EventDate DATE);"
+	ddl[6] = "ALTER TABLE Transactions ADD CONSTRAINT Transactions_FK_Account FOREIGN KEY (Account_ID) REFERENCES Accounts(Account_ID);"
+	ddl[7] = "ALTER TABLE Transactions ADD CONSTRAINT Transactions_FK_OperationsType FOREIGN KEY (OperationsType_ID) REFERENCES OperationsTypes(OperationsType_ID);"
+	ddl[8] = "ALTER TABLE Accounts ADD CONSTRAINT Accounts_UN UNIQUE KEY (Document_Number);"
+	ddl[9] = "INSERT INTO Accounts (Document_Number) VALUES ('123456');"
 	ddl[10] = "INSERT INTO OperationsTypes (OperationsType_ID, Description,OperationsType) VALUES (1, 'COMPRA A VISTA','DEBIT');"
 	ddl[11] = "INSERT INTO OperationsTypes (OperationsType_ID, Description,OperationsType) VALUES (2, 'COMPRA PARCELADA','DEBIT');"
 	ddl[12] = "INSERT INTO OperationsTypes (OperationsType_ID, Description,OperationsType) VALUES (3, 'SAQUE','DEBIT');"
@@ -67,12 +67,16 @@ func before() *sql.DB{
 		log.Fatalf("Can't connet to H2 Database: %s", err)
 	}
 
+	tx, _ := conn.Begin()
 	for i := 0; i < len(ddl); i++ {
-		_, err := conn.Exec(ddl[i])
+		log.Printf("Execute %s", ddl[i])
+		response, err := tx.Exec(ddl[i])
+		log.Printf("Response %v", response)
 		if err != nil {
 			log.Fatalf("Can't exec ddl commands: %s", err)
 		}
 	}
+	tx.Commit()
 	return conn
 }
 
